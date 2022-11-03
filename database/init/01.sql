cd61085c4fd0aa76ee9eb54fb93a2eb9630d6216
Marcelo <marcelo3macedo@gmail.com>
2022-11-03 08:25:43 -0300

    feat: Prisma Setup

---
+++ database/init/01.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `syslibrary`;
+
+USE `syslibrary`;
+GRANT ALL PRIVILEGES ON *.* TO 'docker'@'%';
---
+++ prisma/migrations/20221028193023_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `Author` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Book` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `authorId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Book` ADD CONSTRAINT `Book_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `Author`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
